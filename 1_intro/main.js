014c6a221ebef4c80b01cd844c1f4f4fe5a80663
LukianetsLiza <127424259+LukianetsLiza@users.noreply.github.com>
2023-03-21 00:42:00 +0200

    1 Task

---
+++ 1_intro/main.js
@@ -0,0 +1,23 @@
+// ------------- browser dialogs -----------
+alert("Some alert message!");
+
+// -------------- 1 --------------------
+const userage = prompt('Enter your year of birth:');
+alert(`Your age ${2023-userage}!`);
+
+// -------------- 2 --------------------
+const radius = prompt('Enter circle radius:');
+alert(`Area of ​​a circle ${2*3.14*radius}!`);
+
+// -------------- 3 --------------------
+const distance = prompt('Distance between two cities:');
+const time = prompt('The time you have to reach your destination:');
+alert(`The speed at which you must move ${distance/time}!`);
+
+// -------------- 4 --------------------
+const dollars = prompt('Enter dollars:');
+alert(`It will be ${dollars*0.94} euros!`);
+
+// -------------- 5 --------------------
+const capacity = prompt('Enter the capacity of the flash drive:');
+alert(`${Math.floor(capacity*1024/820)} files with a size of 820 MB can be saved on a flash drive!`);
