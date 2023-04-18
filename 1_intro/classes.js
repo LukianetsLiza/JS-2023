59d84a60fb736a08808711329c92c3b143fa4bba
LukianetsLiza <127424259+LukianetsLiza@users.noreply.github.com>
2023-04-18 22:49:36 +0300

    Task 6 class Button

--- 1_intro/classes.js
+++ 1_intro/classes.js
@@ -41,3 +41,49 @@ console.log(center);
 center.styleAdd();
 center.styleDel();
 center.getCSS();
+
+// ------------- Task 2 -----------
+class Button {
+    constructor(width, height, text) {
+        this.buttonWidth = width;
+        this.buttonHeight = height;
+        this.buttonText = text;
+    }
+
+    showBtn() {
+        document.write(`<button class="button"></button>`);
+    }
+}
+
+let buttonHTML = new Button(65, 30, "Agree");
+
+console.log(buttonHTML);
+buttonHTML.showBtn();
+
+class BootstrapButton extends Button {
+    constructor(width, height, text, color) {
+        super(width, height, text);
+        this.buttonColor = color;
+    }
+
+    showBtn() {
+        super.showBtn();
+        let styleSheet = document.createElement("style");
+        let styles = `
+        button{
+            min-width: ${this.buttonWidth}px;
+            min-height: ${this.buttonHeight}px;
+            background-color: ${this.buttonColor};
+            }
+            button:after{
+                content: "${this.buttonText}";
+        }
+        `;
+        styleSheet.innerText = styles;
+        document.head.appendChild(styleSheet);
+    }
+}
+
+let styledButton = new BootstrapButton(65, 30, "Agree", "green");
+styledButton.showBtn();
+console.log(styledButton);
