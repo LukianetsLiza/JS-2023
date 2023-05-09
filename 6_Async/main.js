866ed627b3d81c99fb340d0b88a90feeaf2c08e0
LukianetsLiza <127424259+LukianetsLiza@users.noreply.github.com>
2023-05-10 00:45:03 +0300

    Task 10 Async

---
+++ 6_Async/main.js
@@ -0,0 +1,27 @@
+function getPromise(message, delay) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(message);
+        }, delay);
+    });
+}
+
+getPromise('test promise', 2000)
+    .then(resolve => console.log(resolve));
+
+function calcArrProduct(arr) {
+    return new Promise((resolve, reject) => {
+        const checkNumber = arr.every(i => ((typeof i) === 'number'));
+        if (checkNumber) {
+            const productArray = arr.reduce((acc, curr) => acc*curr, 1);
+            resolve(productArray);
+        }
+        else {
+            reject('Error! Incorrect array!');
+        }
+    });
+}
+
+calcArrProduct([1, 2, 3])
+    .then(resolve => console.log(resolve))
+    .catch(reject => console.log(reject));
